a5b9ab9dcf9e380c18c9ca1349c8dd0494ca2d65
Oby Magid <xobyxm@hotmail.com>
2019-09-10 03:05:33 +0000

    Update circleCiEnvironmentSetup.sh

--- simple-rt-android/circleCiEnvironmentSetup.sh
+++ simple-rt-android/circleCiEnvironmentSetup.sh
@@ -1,15 +1,15 @@
 function getAndroidNDK {
   ANDROID_HOME="/opt/android/sdk"
-  ANDROID_NDK="$ANDROID_HOME/android-ndk-r16b"
-  echo 'export ANDROID_NDK=$ANDROID_HOME/android-ndk-r16b' >> $BASH_ENV
+  ANDROID_NDK="$ANDROID_HOME/android-ndk-r14"
+  echo 'export ANDROID_NDK=$ANDROID_HOME/android-ndk-r14' >> $BASH_ENV
   echo 'export ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle/' >> $BASH_ENV
 
   if [ ! -e ANDROID_NDK ]; then
     cd $ANDROID_HOME
     echo "Downloading NDK..."
-    sudo curl -L -o ndk.zip https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip
+    sudo curl -L -o ndk.zip https://dl.google.com/android/repository/android-ndk-r14-linux-x86_64.zip
     unzip -o -q ndk.zip
-    mv android-ndk-r16b ndk-bundle
+    mv android-ndk-r14 ndk-bundle
 
     echo "Installed Android NDK at $ANDROID_NDK"
 
